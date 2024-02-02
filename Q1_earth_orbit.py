193f573322f7e099e323ae81b5284728b5738f2b
Gal <gal.yehoshua@gmail.com>
2024-02-02 23:06:15 +0200

    cosmetics

--- Q1_earth_orbit.py
+++ Q1_earth_orbit.py
@@ -7,8 +7,17 @@ def initial_conditions(a, e):
     return 0, a * (1 - e), -(gm / a * (1 + e) / (1 - e)), 0
 
 
-# not depend on time explicitly.
+def compute_single_step_euler(x, y, vx, vy, dt):
+    gm = 4 * np.pi
+    one_over_r3 = (x ** 2 + y ** 2) ** -1.5
+    return (x + dt * vx,
+            y + dt * vy,
+            vx - dt * gm * x * one_over_r3,
+            vy - dt * gm * y * one_over_r3
+            )
 
+
+# not depend on time explicitly.
 def f(location_velocity_vector):
     assert len(location_velocity_vector) == 4, f"bad vector, {location_velocity_vector}"
     x, y, vx, vy = location_velocity_vector
@@ -23,16 +32,6 @@ def f(location_velocity_vector):
     )
 
 
-def compute_single_step_euler(x, y, vx, vy, dt):
-    gm = 4 * np.pi
-    one_over_r3 = (x ** 2 + y ** 2) ** -1.5
-    return (x + dt * vx,
-            y + dt * vy,
-            vx - dt * gm * x * one_over_r3,
-            vy - dt * gm * y * one_over_r3
-            )
-
-
 def compute_single_step_rk4(x, y, vx, vy, dt):
     location_velocity_vector = [x, y, vx, vy]
     k1 = dt * f(location_velocity_vector)
@@ -42,7 +41,7 @@ def compute_single_step_rk4(x, y, vx, vy, dt):
     return np.array(location_velocity_vector) + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
 
-def earth_orbit_euler():
+def earth_orbit(step_method=compute_single_step_euler):
     steps = 1000
     dt = 1.0 / steps
     a, e = 1, 0.1
@@ -53,25 +52,17 @@ def earth_orbit_euler():
         yt.append(y)
         vxt.append(vx)
         vyt.append(vyt)
-        x, y, vx, vy = compute_single_step_euler(x, y, vx, vy, dt)
+        x, y, vx, vy = step_method(x, y, vx, vy, dt)
 
     return x, y, vx, vy
+
+
+def earth_orbit_euler():
+    return earth_orbit(compute_single_step_euler)
 
 
 def earth_orbit_rk4():
-    steps = 1000
-    dt = 1.0 / steps
-    a, e = 1, 0.1
-    x, y, vx, vy = initial_conditions(a, e)
-    xt, yt, vxt, vyt = [], [], [], []
-    for i in range(steps):
-        xt.append(x)
-        yt.append(y)
-        vxt.append(vx)
-        vyt.append(vyt)
-        x, y, vx, vy = compute_single_step_rk4(x, y, vx, vy, dt)
-
-    return x, y, vx, vy
+    return earth_orbit(compute_single_step_rk4)
 
 
 if __name__ == "__main__":
